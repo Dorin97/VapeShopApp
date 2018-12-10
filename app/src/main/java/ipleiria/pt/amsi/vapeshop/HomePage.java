6a5d5cb4e0dd4fb2dbf2d85fe18928bea687c7ad
Dorin Plamadeala <dorin.plamadeala@gmail.com>
2018-12-10 16:57:53 +0000

    home

--- app/src/main/java/ipleiria/pt/amsi/vapeshop/HomePage.java
+++ app/src/main/java/ipleiria/pt/amsi/vapeshop/HomePage.java
@@ -1,5 +1,13 @@
 package ipleiria.pt.amsi.vapeshop;
 
-public class HomePage {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
 }
