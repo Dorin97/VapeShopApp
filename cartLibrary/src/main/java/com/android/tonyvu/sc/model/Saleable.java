81b861cb28c7ce3c134e96d9a19388bb3392317a
Dorin Plamadeala <dorin.plamadeala@gmail.com>
2019-01-28 13:55:29 +0000

    bd helper

--- cartLibrary/src/main/java/com/android/tonyvu/sc/model/Saleable.java
+++ cartLibrary/src/main/java/com/android/tonyvu/sc/model/Saleable.java
@@ -1,12 +1,10 @@
 package com.android.tonyvu.sc.model;
 
-import java.math.BigDecimal;
-
 /**
  * Implements this interface for any product which can be added to shopping cart
  */
 public interface Saleable {
-    BigDecimal getPrice();
+    Float getPrice();
 
     String getName();
 }
